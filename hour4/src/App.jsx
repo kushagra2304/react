225c3731c74ea0aee7636a41315e80194f801466
Kushagra <kushagrasharma826@gmail.com>
2026-01-09 18:05:43 +0530

    hour4

---
+++ hour4/src/App.jsx
@@ -0,0 +1,44 @@
+//todo list
+import { useState } from "react";
+
+function TodoApp() {
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState("");
+
+  const addTodo = () => {
+    if (!input.trim()) return;
+
+    setTodos([...todos, input]);
+    setInput("");
+  };
+
+  const deleteTodo = (index) => {
+    setTodos(todos.filter((_, i) => i !== index));
+  };
+
+  return (
+    <div>
+      <h2>Todo List</h2>
+
+      <input
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+
+      <button onClick={addTodo}>Add</button>
+
+      {todos.length === 0 && <p>No todos yet</p>}
+
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo}
+            <button onClick={() => deleteTodo(index)}>❌</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default TodoApp;
